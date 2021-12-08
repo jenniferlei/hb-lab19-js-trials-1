dd55027d2d964ea097b44a01c76fe3fc39b56d0a
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-08 04:21:39 +0000

    Completed print_as_numbered_list function on trials.py

--- trials.py
+++ trials.py
@@ -43,7 +43,19 @@ def get_odd_indices(items):
 
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    """Given an array, output a numbered list.
+
+    >>> print_as_numbered_list([1, 'hello', True]);
+    1. 1
+    2. hello
+    3. True
+    """
+    i = 1
+
+    for item in items:
+        print(f'{i}. {item}')
+
+        i += 1
 
 
 def get_range(start, stop):
